a25dce2cc266942a09c6b25a43a8c16c93bdf7e1
Ben Bulent Basaran <ben@scala.org>
2024-11-13 20:16:21 -0500

    add a unit test, fix a bug (6 = 2 x 3) and some minor clean up

--- e3/e3a.cpp
+++ e3/e3a.cpp
@@ -6,29 +6,27 @@ void yaz(long);
 void asalÇarpanlarıYaz(long);
 
 int main() {
-  long dene[] = {81, 98, 121, 2048, 13195, 600851475143l};
+  long dene[] = {1, 4, 5, 6, 7, 8, 9, 10, 12, 36, 81, 98, 121, 2048, 13195, 600851475143l};
   // https://stackoverflow.com/questions/20234898/more-modern-way-of-looping-through-c-arrays
-  for(auto s: dene) {
-    asalÇarpanlarıYaz(s);
-  }
+  for(auto s: dene) asalÇarpanlarıYaz(s);
   return 0;
 }
 
-void yaz(long s) { cout << "\t" << s << endl; }
+void yaz(long s) { cout << s << " "; }
 
 void asalÇarpanlarıYaz(long s) {
-  cout << s << ":" << endl;
-  if (s < 2) return;
-  else if (s < 4) {
-    yaz(s);
+  cout << s << ": ";
+  if(s < 4) {
+    cout << endl;
     return;
   }
   long bolen{2};
   float sinir = sqrt(s);
+  long sayi = s;
   do {
     long kalan = s % bolen;
     long carpan = 1;
-    while (kalan == 0 && carpan <= sinir) {
+    while (kalan == 0) {
       carpan *= bolen;
       if (carpan == bolen) { yaz(carpan); }
       s /= bolen;
@@ -38,5 +36,7 @@ void asalÇarpanlarıYaz(long s) {
     else bolen += 2;
     kalan = s % bolen;
   } while(bolen <= sinir);
+  if (s > 1 && s != sayi) yaz(s);
+  cout << endl;
   return;
 }
